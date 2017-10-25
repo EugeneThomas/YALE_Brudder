20bc6f9a686da8e55a4b0ca43f8695881683fbec
TakingTheL <yliu6@stuy.edu>
2017-10-25 13:08:07 -0400

    implemented new_post, get_post, get_all_post, and acc_auth

--- util/database.py
+++ util/database.py
@@ -26,15 +26,25 @@ def initialize_tables():
 def timestamp():
     return('Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()))
 
-def new_post(name, title, content):
+#to make a new post
+def new_post(name, title, content,logged_in):
     if(logged_in):
         command = 'INSERT INTO blog VALUES(%s, %s, %s, %s)'.format(name,title,content,timestamp())
     else:
         redirect(url_for(login))
 
+#to retrieve all posts from a user
 def get_post(name):
+    command = "SELECT * FROM  blog;"
+    return c.execute(command)
 
+#to retrieve all post titles and username
 def get_all_post():
-
-def ret_acc_pass(username, password):
-    command = "SELECT FROM accounts
+    command = "SELECT post_title, blog.name FROM blog;"
+    return c.execute(command)
+    
+def acc_auth(username, password):
+    command = "SELECT username, password FROM accounts WHERE username=\""+username+"\", password =\""+password+"\";"
+    if c.execute(command) == null:
+        return False
+    return True
