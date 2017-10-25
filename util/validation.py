d43e95dc12682d6ffe94db5fe93968c7c45b19e1
Yuyang Zhang <yzhang4@stuy.edu>
2017-10-25 17:40:40 -0400

    Added compare(password)

--- util/validation.py
+++ util/validation.py
@@ -10,5 +10,13 @@ def compare(username):
         if row[0] = username:
             return True
     return False
-        
+
+
+def compare(password):
+    command = "SELECT password FROM accounts;"
+    user_retrieve = c.execute(command)
+    for row in user_retrieve:
+        if row[0] = password:
+            return True
+    return False       
 
