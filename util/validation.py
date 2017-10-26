e7d011a039e29f8cfb82ce9545f770a930bfb003
Leo <yliu6@stuy.edu>
2017-10-26 18:17:02 -0400

    added docstring

--- util/validation.py
+++ util/validation.py
@@ -23,9 +23,21 @@ def compare(password):
 '''
 
 #validate credentials
-#ret_val[0] boolean, whether or not login was successful
-#ret_val[1] string , if ret_val[0] is false, error message, else, the username
+
 def validate(username, password):
+    '''
+    Validate credentials
+    
+    Validate credentials and generate error message if necessary
+    
+    args:
+    username (str): username of the account
+    password (str): password of the account
+
+    rets:
+    ret_val[0] boolean, whether or not login was successful
+    ret_val[1] string , if ret_val[0] is false, error message, else, the username
+    '''
     command = "SELECT username FROM accounts WHERE username={0}".format(username)
     user_retrieved = c.execute(command)
     if (user_retrieved = None):
