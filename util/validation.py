13eca36894597392b19ebd7976927f7a10a7d70e
TakingTheL <yliu6@stuy.edu>
2017-10-26 13:02:05 -0400

    completed validation.py

--- util/validation.py
+++ util/validation.py
@@ -3,6 +3,7 @@ import sqlite3
 db = sqlite3.connect("YALE_BRUDDER")
 c = db.cursor()
 
+'''
 def compare(username):
     command = "SELECT username FROM accounts;"
     user_retrieve = c.execute(command)
@@ -19,4 +20,18 @@ def compare(password):
         if row[0] = password:
             return True
     return False       
+'''
 
+#validate credentials
+#ret_val[0] boolean, whether or not login was successful
+#ret_val[1] string , if ret_val[0] is false, error message, else, the username
+def validate(username, password):
+    command = "SELECT username FROM accounts WHERE username={0}".format(username)
+    user_retrieved = c.execute(command)
+    if (user_retrieved = None):
+        return [False,"Invalid username"]
+    command = "SELECT username FROM accounts WHERE usename={0}, password={1}".format(username, password)
+    user_retrieved = c.execute(command)
+    if (user_retrieved = None):
+        return [False,"Wrong password"]
+    return [True, username]
